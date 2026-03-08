0d6998077c870e35aa3ab948aa04fc7b3c27aaa3
onlyRaghav <raghav2211981@gmail.com>
2026-03-08 22:00:54 +0530

    feat: wired Login register and Dashboard to main App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./pages/Home";
-export default function App(){
-  return(
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Register from "./pages/Register.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+
+export default function App() {
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<ProtectedRoute >
+          <Dashboard />
+        </ProtectedRoute>}
+        />
       </Routes>
     </BrowserRouter>
   )
