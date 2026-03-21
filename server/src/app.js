9390a0a0041f3cf21dba2c962de3117b6a4591d7
onlyRaghav <raghav2211981@gmail.com>
2026-03-21 14:43:13 +0530

    feat: add listing router to the main app

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import CORS from 'cors';
 import HealthRouter from "./routes/health.routes.js";
 import AuthRouter from "./routes/auth.routes.js";
+import ListingRouter from "./routes/listing.routes.js";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use('/api/health', HealthRouter);
 app.use('/api/auth',AuthRouter);
+app.use('/api/listings',ListingRouter);
 
 app.use((req,res)=>{
     res.status(404).json({message:"request out reach"});
