7efc5d0be8df30b2e6552912b7ffa118527a361b
onlyRaghav <raghav2211981@gmail.com>
2026-03-25 12:25:58 +0530

    feat: add feature to remove or update listing

--- server/src/controllers/listing.controller.js
+++ server/src/controllers/listing.controller.js
@@ -22,42 +22,107 @@ export const createListing = async (req, res) => {
             message: "listing created successfully",
             listing,
         })
-    }catch(err){
-        console.error("Error while creating listing:",err)
+    } catch (err) {
+        console.error("Error while creating listing:", err)
         res.status(500).json({
-            message:"Server error"
+            message: "Server error"
         })
     }
 }
 
-export const getAllListings= async (req,res)=>{
-    try{
-        const getListings= await listingModel.find().populate("host","name email");
+export const getAllListings = async (req, res) => {
+    try {
+        const getListings = await listingModel.find().populate("host", "name email");
 
         res.status(200).json(getListings);
 
-    }catch(error){
-        console.error("Error while fetching listings:",error);
+    } catch (error) {
+        console.error("Error while fetching listings:", error);
         res.status(500).json({
-            message:"server error"
+            message: "server error"
         })
     }
 }
 
-export const getListingsById= async (req,res)=>{
-    try{
-        const {id}= req.params;
-        const listings = await listingModel.findById(id).populate("host","name email");
-        if(!listings){
+export const getListingsById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const listings = await listingModel.findById(id).populate("host", "name email");
+        if (!listings) {
             res.status(404).json({
-                message:"listing not found"
+                message: "listing not found"
             })
         }
         res.status(200).json(listings);
-    }catch(error){
-        console.error("Get lisiting error:",error);
+    } catch (error) {
+        console.error("Get lisiting error:", error);
         res.status(500).json({
-            message:"Server error"
+            message: "Server error"
+        })
+    }
+}
+
+export const updateListing = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const listing = await listingModel.findById(id);
+
+        if (!listing) {
+            return res.status(404).json({
+                message: "listing not found"
+            })
+        }
+        if (listingModel.host.toString !== req.user._id.toString()) {
+            return res.status(403).json({
+                message: "Not Authorized to update this listing"
+            })
+        }
+
+        const { title, description, location, images, price } = req.body;
+
+        if (title !== undefined) listing.title = title;
+        if (description !== undefined) listing.description = description;
+        if (location !== undefined) listing.location = location;
+        if (price !== undefined) listing.price = price;
+        if (images !== undefined) listing.images = images;
+
+        const updatedListing = await listing.save();
+
+        res.status(200).json({
+            message: "Listing updated successfully",
+            listing: updatedListing
+        })
+    } catch (err) {
+        console.error("Updating Listind Error:", err);
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+}
+
+export const deleteListing = async (req, res) => {
+
+    try {
+        const { id } = req.params;
+        const listing = await listingModel.findById(id);
+        if (!listing) {
+            return res.status(404).json({
+                message: "listing not found"
+            })
+        }
+        if (req.user._id.toString() == listing.host.toString()) {
+            return res.status(403).json({
+                message: "Not Authorized. Can't perform Operation."
+            })
+        }
+        await listingModel.findByIdAndDelete(id);
+        res.status(200).json({
+            message: "listing deleted successfully"
+        })
+    } catch (err) {
+        console.error("Error removing listing:", err);
+        res.status(500).json({
+            message: "server error"
         })
     }
 }
--- server/src/routes/listing.routes.js
+++ server/src/routes/listing.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
-import { createListing, getAllListings, getListingsById } from "../controllers/listing.controller.js"
+import { createListing, getAllListings, getListingsById, updateListing,deleteListing } from "../controllers/listing.controller.js"
 import { protect } from "../middleware/auth.middleware.js"
 const router = Router();
 
 router.post('/', protect, createListing);
 router.get('/',getAllListings);
 router.get('/:id',getListingsById);
+router.put('/:id',updateListing);
+router.delete('/:id',deleteListing);
 
 export default router;
