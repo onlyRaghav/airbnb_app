51804378bd3403dd07b045f791afa2e82fb1d9cf
onlyRaghav <raghav2211981@gmail.com>
2026-03-25 12:37:15 +0530

    fix: add auth middleware for listing update and delete requests

--- server/src/routes/listing.routes.js
+++ server/src/routes/listing.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express"
-import { createListing, getAllListings, getListingsById, updateListing,deleteListing } from "../controllers/listing.controller.js"
+import { createListing, getAllListings, getListingsById, updateListing, deleteListing } from "../controllers/listing.controller.js"
 import { protect } from "../middleware/auth.middleware.js"
 const router = Router();
 
 router.post('/', protect, createListing);
-router.get('/',getAllListings);
-router.get('/:id',getListingsById);
-router.put('/:id',updateListing);
-router.delete('/:id',deleteListing);
+router.get('/', getAllListings);
+router.get('/:id', getListingsById);
+router.put('/:id', protect, updateListing);
+router.delete('/:id', protect, deleteListing);
 
 export default router;
